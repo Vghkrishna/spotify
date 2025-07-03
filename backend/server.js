e252e281ce7daefaa5a07a4bbf1c67e3d2a68684
krishna vaghela <krishnav@systemintegration.in>
2025-07-03 14:49:01 +0530

    krishna- done with port change

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,7 @@ app.use("*", (req, res) => {
   res.status(404).json({ message: "Route not found" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
